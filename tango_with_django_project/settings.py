cb776e82e596b381d30b18eb327af06eb7699166
Nick Alexiadis <nickalexiadis@outlook.com>
2020-02-17 18:25:45 +0000

    Chapters8&9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -22,6 +22,8 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+LOGIN_URL = 'rango:login'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
